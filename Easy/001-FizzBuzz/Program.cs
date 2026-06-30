4476549b29b4e9e9d1ccd0a69fdb9da519388be8
Eleri Ferreira <107778775+elerijunior@users.noreply.github.com>
2026-06-30 17:21:59 -0300

    chore: initial repository structure

---
+++ Easy/001-FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _001_FizzBuzz;
+
+public class Program
+{
+    public static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+
+        FizzBuzz(num);
+    }
+
+    public static void FizzBuzz(int target)
+    {
+        for (int i = 1; i <= target; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
